a86fc43ddc4ad8bab25907e95041bf587ec4d3bd
mamokebe <kebedebekele7@gmail.com>
2024-11-24 18:47:41 -0600

    Header added

---
+++ react-redux/src/assets/products.js
@@ -0,0 +1,756 @@
+import slider1 from "./images/slider1.webp";
+import slider2 from "./images/slider2.png";
+import slider3 from "./images/slider3.png";
+import slider4 from "./images/slider4.png";
+import slider5 from "./images/slider5.webp";
+import slider6 from "./images/slider6.png";
+//*************************************** */
+import category1 from "./images/category1.jpeg";
+import category2 from "./images/category2.jpeg";
+import category3 from "./images/category3.jpg";
+import category4 from "./images/category4.jpg";
+import category5 from "./images/category5.jpg";
+//***************************************** */
+import products1 from "./images/products1.jpg";
+import products2 from "./images/products2.jpg";
+import products3 from "./images/products3.jpg";
+import products4 from "./images/products4.jpg";
+import products5 from "./images/products5.jpg";
+import products6 from "./images/products6.webp";
+import products7 from "./images/products7.jpg";
+import products8 from "./images/products8.jpg";
+import products9 from "./images/products9.jpg";
+import products10 from "./images/products10.jpg";
+import products11 from "./images/products11.jpg";
+import products12 from "./images/products12.jpg";
+import products13 from "./images/products13.jpg";
+import products14 from "./images/products14.webp";
+import products15 from "./images/products15.jpg";
+import products16 from "./images/products16.webp";
+import products17 from "./images/products17.jpg";
+import products18 from "./images/products18.webp";
+import products19 from "./images/products19.webp";
+import products20 from "./images/products20.jpg";
+import products21 from "./images/products21.jpg";
+import products22 from "./images/products22.jpg";
+import products23 from "./images/products23.jpg";
+import products24 from "./images/products24.jpg";
+import products25 from "./images/products25.jpg";
+import products26 from "./images/products26.webp";
+import products27 from "./images/products27.jpg";
+import products28 from "./images/products28.jpg";
+import products29 from "./images/products29.jpg";
+import products30 from "./images/products30.webp";
+import products31 from "./images/products31.jpg";
+import products32 from "./images/products32.jpg";
+import products33 from "./images/products33.webp";
+import products34 from "./images/products34.jpeg";
+import products35 from "./images/products35.jpg";
+import products36 from "./images/products36.jpg";
+import products37 from "./images/products37.webp";
+import products38 from "./images/products38.webp";
+import products39 from "./images/products39.jpg";
+import products40 from "./images/products40.jpg";
+import products41 from "./images/products41.jpg";
+import products42 from "./images/products42.jpg";
+import products43 from "./images/products43.jpg";
+import products44 from "./images/products44.jpg";
+//******************************************** */
+export const sliderItems = [
+  {
+    id: 1,
+    image: slider1,
+    title: "Beauty Collection",
+    desc: "Trending Now: Bold Colors and Prints. Up to 30% Off Selected Items",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    image: slider2,
+    title: "Football Trend",
+    desc: "Best Sellers: Sport with necessary wear. Don't Miss Out!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    image: slider3,
+    title: "Delicious with Essentials ",
+    desc: "Get it best kitchen items with discount. Up to 50% Off Selected items",
+    bg: "fbf0f4",
+  },
+  {
+    id: 4,
+    image: slider4,
+    title: "Fall Collection",
+    desc: "Elevate Your Style with Our Latest Collection. Up to 40% Off Selected Items",
+    bg: "f5fafd",
+  },
+  {
+    id: 5,
+    image: slider5,
+    title: "Spring Trend",
+    desc: "Best Sellers: Must-Have Wardrobe Pieces. Don't Miss Out!",
+    bg: "fcf1ed",
+  },
+  {
+    id: 6,
+    image: slider6,
+    title: "Summer Essentials",
+    desc: "Elevate Your Style with Our Latest Collection. Up to 50% Off Selected Items",
+    bg: "fbf0f4",
+  },
+];
+//****************************************** */
+export const categories = [
+  {
+    id: 1,
+    image: category1,
+    title: "Home kits",
+  },
+  {
+    id: 2,
+    image: category2,
+    title: "Clothes",
+  },
+  {
+    id: 3,
+    image: category3,
+    title: "Shoes",
+  },
+  {
+    id: 4,
+    image: category4,
+    title: "Sport kits",
+  },
+  {
+    id: 5,
+    image: category5,
+    title: "Jewelry",
+  },
+];
+//****************************************** */
+export const newArrivals = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: products1,
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+    keywords: ["socks", "sports", "apparel"],
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: products2,
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    priceCents: 2095,
+    keywords: ["sports", "basketballs"],
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: products3,
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+    keywords: ["tshirts", "apparel", "mens"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: products4,
+    name: "2 Slot Toaster - Black",
+    rating: {
+      stars: 5,
+      count: 2197,
+    },
+    priceCents: 1899,
+    keywords: ["toaster", "kitchen", "appliances"],
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: products5,
+    name: "6 Piece White Dinner Plate Set",
+    rating: {
+      stars: 4,
+      count: 37,
+    },
+    priceCents: 2067,
+    keywords: ["plates", "kitchen", "dining"],
+  },
+];
+//****************************************** */
+export const bestsellers = [
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: products6,
+    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    rating: {
+      stars: 4.5,
+      count: 175,
+    },
+    priceCents: 3499,
+    keywords: ["kitchen", "cookware"],
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: products7,
+    name: "Plain Hooded Fleece Sweatshirt",
+    rating: {
+      stars: 4.5,
+      count: 317,
+    },
+    priceCents: 2400,
+    keywords: ["hoodies", "sweaters", "apparel"],
+  },
+  {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: products8,
+    name: "Luxury Towel Set - Graphite Gray",
+    rating: {
+      stars: 4.5,
+      count: 144,
+    },
+    priceCents: 3599,
+    keywords: ["bathroom", "washroom", "restroom", "towels", "bath towels"],
+  },
+  {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: products9,
+    name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
+    rating: {
+      stars: 4.5,
+      count: 305,
+    },
+    priceCents: 2899,
+    keywords: ["bathroom", "cleaning"],
+  },
+  {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: products10,
+    name: "Waterproof Knit Athletic Sneakers - Gray",
+    rating: {
+      stars: 4,
+      count: 89,
+    },
+    priceCents: 3390,
+    keywords: ["shoes", "running shoes", "footwear"],
+  },
+];
+//****************************************** */
+export const allProducts = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: products1,
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87,
+    },
+    priceCents: 1090,
+    keywords: ["socks", "sports", "apparel"],
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: products2,
+    name: "Intermediate Size Basketball",
+    rating: {
+      stars: 4,
+      count: 127,
+    },
+    priceCents: 2095,
+    keywords: ["sports", "basketballs"],
+  },
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: products3,
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56,
+    },
+    priceCents: 799,
+    keywords: ["tshirts", "apparel", "mens"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+  {
+    id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    image: products4,
+    name: "2 Slot Toaster - Black",
+    rating: {
+      stars: 5,
+      count: 2197,
+    },
+    priceCents: 1899,
+    keywords: ["toaster", "kitchen", "appliances"],
+  },
+  {
+    id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image: products5,
+    name: "6 Piece White Dinner Plate Set",
+    rating: {
+      stars: 4,
+      count: 37,
+    },
+    priceCents: 2067,
+    keywords: ["plates", "kitchen", "dining"],
+  },
+  {
+    id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    image: products6,
+    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    rating: {
+      stars: 4.5,
+      count: 175,
+    },
+    priceCents: 3499,
+    keywords: ["kitchen", "cookware"],
+  },
+  {
+    id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    image: products7,
+    name: "Plain Hooded Fleece Sweatshirt",
+    rating: {
+      stars: 4.5,
+      count: 317,
+    },
+    priceCents: 2400,
+    keywords: ["hoodies", "sweaters", "apparel"],
+  },
+  {
+    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+    image: products8,
+    name: "Luxury Towel Set - Graphite Gray",
+    rating: {
+      stars: 4.5,
+      count: 144,
+    },
+    priceCents: 3599,
+    keywords: ["bathroom", "washroom", "restroom", "towels", "bath towels"],
+  },
+  {
+    id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image: products9,
+    name: "Liquid Laundry Detergent, 110 Loads, 82.5 Fl Oz",
+    rating: {
+      stars: 4.5,
+      count: 305,
+    },
+    priceCents: 2899,
+    keywords: ["bathroom", "cleaning"],
+  },
+  {
+    id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+    image: products10,
+    name: "Waterproof Knit Athletic Sneakers - Gray",
+    rating: {
+      stars: 4,
+      count: 89,
+    },
+    priceCents: 3390,
+    keywords: ["shoes", "running shoes", "footwear"],
+  },
+  {
+    id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+    image: products11,
+    name: "Women's Chiffon Beachwear Cover Up - Black",
+    rating: {
+      stars: 4.5,
+      count: 235,
+    },
+    priceCents: 2070,
+    keywords: ["robe", "swimsuit", "swimming", "bathing", "apparel"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+  {
+    id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+    image: products12,
+    name: "Round Sunglasses",
+    rating: {
+      stars: 4.5,
+      count: 30,
+    },
+    priceCents: 1560,
+    keywords: ["accessories", "shades"],
+  },
+  {
+    id: "04701903-bc79-49c6-bc11-1af7e3651358",
+    image: products13,
+    name: "Women's Two Strap Buckle Sandals - Tan",
+    rating: {
+      stars: 4.5,
+      count: 562,
+    },
+    priceCents: 2499,
+    keywords: ["footwear", "sandals", "womens", "beach", "summer"],
+  },
+  {
+    id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+    image: products14,
+    name: "Blackout Curtains Set 4-Pack - Beige",
+    rating: {
+      stars: 4.5,
+      count: 232,
+    },
+    priceCents: 4599,
+    keywords: ["bedroom", "curtains", "home"],
+  },
+  {
+    id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+    image: products15,
+    name: "Men's Slim-Fit Summer Shorts",
+    rating: {
+      stars: 4,
+      count: 160,
+    },
+    priceCents: 1699,
+    keywords: ["shorts", "apparel", "mens"],
+  },
+  {
+    id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+    image: products16,
+    name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
+    rating: {
+      stars: 5,
+      count: 846,
+    },
+    priceCents: 3074,
+    keywords: ["water boiler", "appliances", "kitchen"],
+  },
+  {
+    id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+    image: products17,
+    name: "Ultra Soft Tissue 2-Ply - 18 Box",
+    rating: {
+      stars: 4,
+      count: 99,
+    },
+    priceCents: 2374,
+    keywords: ["kleenex", "tissues", "kitchen", "tissues box", "napkins"],
+  },
+  {
+    id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+    image: products18,
+    name: "Straw Lifeguard Sun Hat",
+    rating: {
+      stars: 4,
+      count: 215,
+    },
+    priceCents: 2200,
+    keywords: ["hats", "straw hats", "summer", "apparel"],
+  },
+  {
+    id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+    image: products19,
+    name: "Sterling Silver Sky Flower Stud Earrings",
+    rating: {
+      stars: 4.5,
+      count: 52,
+    },
+    priceCents: 1799,
+    keywords: ["jewelry", "accessories", "womens"],
+  },
+  {
+    id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+    image: products20,
+    name: "Women's Stretch Popover Hoodie",
+    rating: {
+      stars: 4.5,
+      count: 2465,
+    },
+    priceCents: 1374,
+    keywords: ["hooded", "hoodies", "sweaters", "womens", "apparel"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+  {
+    id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+    image: products21,
+    name: "Bathroom Bath Rug Mat 20 x 31 Inch - Grey",
+    rating: {
+      stars: 4.5,
+      count: 119,
+    },
+    priceCents: 1250,
+    keywords: ["bathmat", "bathroom", "home"],
+  },
+  {
+    id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+    image: products22,
+    name: "Women's Knit Ballet Flat",
+    rating: {
+      stars: 4,
+      count: 326,
+    },
+    priceCents: 2640,
+    keywords: ["shoes", "flats", "womens", "footwear"],
+  },
+  {
+    id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+    image: products23,
+    name: "Men's Regular-Fit Quick-Dry Golf Polo Shirt",
+    rating: {
+      stars: 4.5,
+      count: 2556,
+    },
+    priceCents: 1599,
+    keywords: ["tshirts", "shirts", "apparel", "mens"],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png",
+  },
+  {
+    id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+    image: products24,
+    name: "Trash Can with Foot Pedal - Brushed Stainless Steel",
+    rating: {
+      stars: 4.5,
+      count: 2286,
+    },
+    priceCents: 8300,
+    keywords: ["garbage", "bins", "cans", "kitchen"],
+  },
+  {
+    id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+    image: products25,
+    name: "Duvet Cover Set with Zipper Closure",
+    rating: {
+      stars: 4,
+      count: 456,
+    },
+    priceCents: 2399,
+    keywords: ["bedroom", "bed sheets", "sheets", "covers", "home"],
+  },
+  {
+    id: "d2785924-743d-49b3-8f03-ec258e640503",
+    image: products26,
+    name: "Women's Chunky Cable Beanie - Gray",
+    rating: {
+      stars: 5,
+      count: 83,
+    },
+    priceCents: 1250,
+    keywords: ["hats", "winter hats", "beanies", "tuques", "apparel", "womens"],
+  },
+  {
+    id: "ee1f7c56-f977-40a4-9642-12ba5072e2b0",
+    image: products27,
+    name: "Men's Classic-fit Pleated Chino Pants",
+    rating: {
+      stars: 4.5,
+      count: 9017,
+    },
+    priceCents: 2290,
+    keywords: ["pants", "apparel", "mens"],
+  },
+  {
+    id: "1c079479-8586-494f-ab53-219325432536",
+    image: products28,
+    name: "Men's Athletic Sneaker",
+    rating: {
+      stars: 4,
+      count: 229,
+    },
+    priceCents: 3890,
+    keywords: ["shoes", "running shoes", "footwear", "mens"],
+  },
+  {
+    id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+    image: products29,
+    name: "Men's Navigator Sunglasses Pilot",
+    rating: {
+      stars: 3.5,
+      count: 42,
+    },
+    priceCents: 1690,
+    keywords: ["sunglasses", "glasses", "accessories", "shades"],
+  },
+  {
+    id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+    image: products30,
+    name: "Non-Stick Cookware Set, Pots, Pans and Utensils - 15 Pieces",
+    rating: {
+      stars: 4.5,
+      count: 511,
+    },
+    priceCents: 6797,
+    keywords: ["cooking set", "kitchen"],
+  },
+  {
+    id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+    image: products31,
+    name: "Vanity Mirror with Heavy Base - Chrome",
+    rating: {
+      stars: 4.5,
+      count: 130,
+    },
+    priceCents: 1649,
+    keywords: ["bathroom", "washroom", "mirrors", "home"],
+  },
+  {
+    id: "a45cfa0a-66d6-4dc7-9475-e2b01595f7d7",
+    image: products32,
+    name: "Women's Fleece Jogger Sweatpant",
+    rating: {
+      stars: 4.5,
+      count: 248,
+    },
+    priceCents: 2400,
+    keywords: ["pants", "sweatpants", "jogging", "apparel", "womens"],
+  },
+  {
+    id: "d339adf3-e004-4c20-a120-40e8874c66cb",
+    image: products33,
+    name: "Double Oval Twist French Wire Earrings - Gold",
+    rating: {
+      stars: 4.5,
+      count: 117,
+    },
+    priceCents: 2400,
+    keywords: ["accessories", "womens"],
+  },
+  {
+    id: "d37a651a-d501-483b-aae6-a9659b0757a0",
+    image: products34,
+    name: "Round Airtight Food Storage Containers - 5 Piece",
+    rating: {
+      stars: 4,
+      count: 126,
+    },
+    priceCents: 2899,
+    keywords: ["boxes", "food containers", "kitchen"],
+  },
+  {
+    id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+    image: products35,
+    name: "Coffeemaker with Glass Carafe and Reusable Filter - 25 Oz, Black",
+    rating: {
+      stars: 4.5,
+      count: 1211,
+    },
+    priceCents: 2250,
+    keywords: ["coffeemakers", "kitchen", "appliances"],
+  },
+  {
+    id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
+    image: products36,
+    name: "Blackout Curtains Set 42 x 84-Inch - Black, 2 Panels",
+    rating: {
+      stars: 4.5,
+      count: 363,
+    },
+    priceCents: 3099,
+    keywords: ["bedroom", "home"],
+  },
+  {
+    id: "8a53b080-6d40-4a65-ab26-b24ecf700bce",
+    image: products37,
+    name: "100% Cotton Bath Towels - 2 Pack, Light Teal",
+    rating: {
+      stars: 4.5,
+      count: 93,
+    },
+    priceCents: 2110,
+    keywords: ["bathroom", "home", "towels"],
+  },
+  {
+    id: "10ed8504-57db-433c-b0a3-fc71a35c88a1",
+    image: products38,
+    name: "Waterproof Knit Athletic Sneakers - Pink",
+    rating: {
+      stars: 4,
+      count: 89,
+    },
+    priceCents: 3390,
+    keywords: ["shoes", "running shoes", "footwear", "womens"],
+  },
+  {
+    id: "77a845b1-16ed-4eac-bdf9-5b591882113d",
+    image: products39,
+    name: "Countertop Blender - 64oz, 1400 Watts",
+    rating: {
+      stars: 4,
+      count: 3,
+    },
+    priceCents: 10747,
+    keywords: ["food blenders", "kitchen", "appliances"],
+  },
+  {
+    id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+    image: products40,
+    name: "10-Piece Mixing Bowl Set with Lids - Floral",
+    rating: {
+      stars: 5,
+      count: 679,
+    },
+    priceCents: 3899,
+    keywords: ["mixing bowls", "baking", "cookware", "kitchen"],
+  },
+  {
+    id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+    image: products41,
+    name: "2-Ply Kitchen Paper Towels - 30 Pack",
+    rating: {
+      stars: 4.5,
+      count: 1045,
+    },
+    priceCents: 5799,
+    keywords: ["kitchen", "kitchen towels", "tissues"],
+  },
+  {
+    id: "bc2847e9-5323-403f-b7cf-57fde044a955",
+    image: products42,
+    name: "Men's Full-Zip Hooded Fleece Sweatshirt",
+    rating: {
+      stars: 4.5,
+      count: 3157,
+    },
+    priceCents: 2400,
+    keywords: ["sweaters", "hoodies", "apparel", "mens"],
+  },
+];
+//****************************************** */
+export const footerLinks = [
+  {
+    id: 1,
+    title: "Home",
+  },
+  {
+    id: 2,
+    title: "Cart",
+  },
+  {
+    id: 3,
+    title: "Sport",
+  },
+  {
+    id: 4,
+    title: "New Fashion",
+  },
+  {
+    id: 5,
+    title: "Jewelry",
+  },
+  {
+    id: 6,
+    title: "My Account",
+  },
+  {
+    id: 7,
+    title: "Order Tracking",
+  },
+  {
+    id: 8,
+    title: "Wishlist",
+  },
+];
--- react-redux/src/components/Header/Header.jsx
+++ react-redux/src/components/Header/Header.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import classes from "./Header.module.css";
+import { GoSearch } from "react-icons/go";
+import { LuShoppingCart } from "react-icons/lu";
 
 const Header = () => {
   return (
-    <div className={classes.Header__container}>
-      <p>Header</p>
+    <div className={classes.header__container}>
+      <div className={classes.header__wrapper}>
+        <div className={classes.header__left}>
+          <h1>ALL-IN-ALL</h1>
+        </div>
+
+        <div className={classes.header__middle}>
+          <div className={classes.search__container}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" placeholder="Search" />
+            <GoSearch size={25} />
+          </div>
+          <select name="" id="">
+            <option value="">EN</option>
+          </select>
+        </div>
+
+        <div className={classes.header__right}>
+          <p className={classes.header__menu}>Register</p>
+          <p className={classes.header__menu}>Login</p>
+          <div className={classes.header__menu}>
+            <LuShoppingCart size={35} />
+            <span className={classes.cart__badge}>0</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
