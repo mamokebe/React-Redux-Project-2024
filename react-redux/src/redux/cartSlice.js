d3c60580724807b51ec8f2e6756d9d156e001cdf
mamokebe <kebedebekele7@gmail.com>
2024-11-30 23:48:01 -0600

    redux toolkit added

---
+++ react-redux/src/redux/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const existingItem = state.cartItems.find(
+        (item) => item.id === action.payload.id
+      );
+      if (!existingItem) {
+        state.cartItems.push({ ...action.payload, quantity: 1 });
+      }
+    },
+  },
+});
